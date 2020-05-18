a8218d10393a52c4d1a7e0437931ff19c8a1986f
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-18 18:59:27 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,7 +13,7 @@ end
 def all_phrases 
   puts "It's-a me, Mario!"
   puts 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts "It's Dangerous To Go Alone! Take This."
+  puts 'It's Dangerous To Go Alone! Take This.'
 end
 
 
